c4cc3bf8aea7ac012f5b7f731dd984a37dc134a2
mhkmcp <mhkmcp@yahoo.com>
2020-07-16 01:38:46 +0600

    Added VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,31 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/dashboard",
+        component: () => import("./components/Dashboard.vue")
+    },
+    { path: "/profile", component: () => import("./components/Profile.vue") }
+];
+/**
+ * Components should be either of
+() => import("./components/Dashboard.vue") 
+or require("./components/Dashboard.vue").default
+**/
+
+const router = new VueRouter({
+    mode: "history",
+    routes // short for `routes: routes`
+});
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,7 +40,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +52,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    router
 });
